e1549d11e5c478b08b1dcacf2b36bd863adfb92f
Mary Lacerda <marylacerda583@gmail.com>
2026-01-11 13:52:42 -0300

    Implementa menu hambúrguer responsivo

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const prevButton = document.getElementById('prev');
-    const nextButton = document.getElementById('next');
-    const container = document.querySelector('.container');
-    const items = container.querySelectorAll('.list .item');
-    const list = container.querySelector('.list');
+  const prevButton = document.getElementById("prev");
+  const nextButton = document.getElementById("next");
+  const items = document.querySelectorAll(".list .item");
 
-    let active = 0;
-    const firstPosition = 0;
-    const lastPosition = items.length - 1;
+  let active = 0;
+  const lastPosition = items.length - 1;
 
-    nextButton.onclick = () => {
-        list.style.setProperty('--calculation', 1)
-        const itemOld = container.querySelector('.list .item.active');
-        if (itemOld) itemOld.classList.remove('active');
+  nextButton.onclick = () => {
+    items[active].classList.remove("active");
+    active = active + 1 > lastPosition ? 0 : active + 1;
+    items[active].classList.add("active");
+  };
 
-        active = active + 1 > lastPosition ? 0 : active + 1;
-        items[active].classList.add('active');
-    };
-
-    prevButton.onclick = () => {
-        list.style.setProperty('--calculation', -1)
-        const itemOld = container.querySelector('.list .item.active');
-        if (itemOld) itemOld.classList.remove('active');
-
-        active = active - 1 < firstPosition ? lastPosition : active - 1;
-        items[active].classList.add('active');
-    };
+  prevButton.onclick = () => {
+    items[active].classList.remove("active");
+    active = active - 1 < 0 ? lastPosition : active - 1;
+    items[active].classList.add("active");
+  };
 });
+
+function showMenu() {
+  const menu = document.getElementById("menu-list");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
